7ce30616b770158d63e76788aad56ce198ba9ac5
Paul Prince <paul@littlebluetech.com>
2017-09-16 07:15:33 +0000

    Add check_isbn() - accepts both ISBN-10 and ISBN-13.

--- isbn.rb
+++ isbn.rb
@@ -21,22 +21,26 @@ def check_isbn13(input)
     return checkdigit == (10 - (digits.map.with_index{ |d,i| (i%2==0) ? d : d*3 }.reduce(:+) % 10)) % 10
 end
 
+def check_isbn(input)
+    return check_isbn10(input) || check_isbn13(input)
+end
+
 puts "Valid ISBN10's:"
-puts check_isbn10('123456789X')
-puts check_isbn10('01 234 567 89')
-puts check_isbn10('0-471-958-697')
+puts check_isbn('123456789X')
+puts check_isbn('01 234 567 89')
+puts check_isbn('0-471-958-697')
 puts
 puts "Invalid ISBN10's:"
-puts check_isbn10('0123456780')
-puts check_isbn10('0-471-958-698')
-puts check_isbn10('123456789x')
+puts check_isbn('0123456780')
+puts check_isbn('0-471-958-698')
+puts check_isbn('123456789x')
 puts
 puts "Valid ISBN13's:"
-puts check_isbn13('9780470059029')
-puts check_isbn13('978-0-13-149505-0')
-puts check_isbn13('978 0 471 48648 0')
+puts check_isbn('9780470059029')
+puts check_isbn('978-0-13-149505-0')
+puts check_isbn('978 0 471 48648 0')
 puts
 puts "Invalid ISBN13's:"
-puts check_isbn13('9780470059020')
-puts check_isbn13('978-0-13-149505-1')
-puts check_isbn13('978_0_471_48648_0')
+puts check_isbn('9780470059020')
+puts check_isbn('978-0-13-149505-1')
+puts check_isbn('978_0_471_48648_0')
